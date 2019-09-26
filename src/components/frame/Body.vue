b744de0edfebf0978cc405a2c986626d7452eaf8
cabalhaoma_sd2082@CEACCS.NET <cabalhaoma_sd2082@CEACCS.NET>
2019-09-26 13:36:15 +0800

    initial commit

---
+++ src/components/frame/Body.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <transition>
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
+<style <style lang="scss" scoped>
+      body{
+    background-image: url(https://www.free-power-point-templates.com/wp-content/uploads/2011/01/910_example.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    } 
+</style>
+
+
+   
